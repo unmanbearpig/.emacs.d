5e64e01e02a91e7dea3229e7d49b3cae10a11825
unmanbearpig <unmanbearpig@gmail.com>
2017-09-26 16:03:52 +0300

    Move customizations into customizations.el

--- init.el
+++ init.el
@@ -8,17 +8,5 @@
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (company-web emmet-mode counsel-projectile yasnippet web-mode use-package sass-mode rspec-mode purescript-mode psc-ide ox-twbs org-projectile multiple-cursors markdown-mode magit inf-ruby hydra helm-ag haskell-mode git-gutter+ crux counsel chruby cargo ag adaptive-wrap))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file "~/.emacs.d/customizations.el")
